6a2128ba46cf28f90289f84d9452fa3ba67f26c4
kuuseke1 <rosannelynx@gmail.com>
2019-05-16 07:01:39 +0300

    add stuff

---
+++ src/components/layout/BasketItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-three-quarters is-offset-2">
+        <div class="columns">
+          <div class="column is-2">
+            <img :src="pattern.picture" />
+          </div>
+          <div class="column is-2">
+            <span>{{ pattern.title }}</span>
+          </div>
+          <div class="column is-2">
+            <span>{{ pattern.price }}</span>
+          </div>
+          <div class="column is-2">
+            <a v-on:click="$emit('deleteItem')">Kustuta</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasketItem",
+  props: ["pattern"]
+};
+</script>
---
+++ src/json/visitordata.js
@@ -0,0 +1,10 @@
+{
+"labels": ["January", "February"],
+"datasets": [
+{
+"label": "Data One",
+"backgroundColor": "#f87979",
+"data": [40, 20]
+}
+]
+}
